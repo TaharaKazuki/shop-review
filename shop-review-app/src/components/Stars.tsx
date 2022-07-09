b320731ba4720c54edc65ee50c3a6d1d5363f871
Tahara Kazuki <>
2022-07-10 00:29:20 +0900

    update

--- shop-review-app/src/components/Stars.tsx
+++ shop-review-app/src/components/Stars.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react'
-import { View, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import { FontAwesome } from '@expo/vector-icons'
 
 type Props = {
@@ -12,6 +12,11 @@ export const Stars: FC<Props> = ({ score, starSize = 16, textSize = 14 }) => {
   return (
     <View style={styles.container}>
       <FontAwesome name="star" style={[styles.star, { fontSize: starSize }]} />
+      <FontAwesome name="star" style={[styles.star, { fontSize: starSize }]} />
+      <FontAwesome name="star" style={[styles.star, { fontSize: starSize }]} />
+      <FontAwesome name="star" style={[styles.star, { fontSize: starSize }]} />
+      <FontAwesome name="star" style={[styles.star, { fontSize: starSize }]} />
+      <Text style={styles.scoreText}>{score}</Text>
     </View>
   )
 }
@@ -19,9 +24,14 @@ export const Stars: FC<Props> = ({ score, starSize = 16, textSize = 14 }) => {
 const styles = StyleSheet.create({
   container: {
     marginTop: 8,
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   star: {
     color: '#900',
     marginRight: 4,
+  },
+  scoreText: {
+    color: '#000',
   },
 })
